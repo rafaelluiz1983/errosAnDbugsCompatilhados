bd686987f6387de1b83e984317312043ec71a0a9
rafaelluiz1983 <77023596+rafaelluiz1983@users.noreply.github.com>
2023-05-19 17:30:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,15 +5,22 @@ function tocaSom (idElementoAudio){
 /* a palavra const é para torar uma declaração constante e não variavel.*/
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let =contador = 0;
+
 
 //emquanto
-while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = function (){
-        tocaSom('#som_tecla_pom');
+for (let contador = 0; contador < listaDeTeclas.length; contador ++ ){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];    
+    const idAudio = `#som_${instrumento}`; // template string
+  //console.log(instrumento);
+  
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
     }
-    contador ++;
-    console.log(contador);
+    //console.log(contador);
+    
 }
 
 
